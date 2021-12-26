2cb3f64f16382ba0356b6b7d53f594023b80a2da
alexandr0 <example@gmail.com>
2021-12-26 12:06:54 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,103 @@
+"use strict";
+// let number = 5;  let переменная может быть изменена.
+// const leftBorderWidth = 1;  const не может быть изменена.
+
+
+
+// Lesson 6
+
+
+// let number = 4.6;
+
+// console.log(-4 / 0);
+// console.log("string" * 9);
+
+// const persone = "Alex";
+
+// const bool = false;
+
+// // console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//   name: "John",
+//   age: 25,
+//   isMarrried: false,
+// };
+
+// // console.log(obj.name);
+// console.log(obj["name"]);
+
+// let arr = ["plum.png", "orange.jpg", 6, "apple.bmp", {}, []];
+// console.log(arr[1]);
+
+
+
+// Lesson 7
+
+
+// alert("Hello");
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Age 18+?", "18"); // Вся информация что приходит от пользователя будет в виде строк 'string'
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('What is your name?', '');
+// answers[1] = prompt('What is your second name?', '');
+// answers[2] = prompt('How old are you?', '');
+
+// console.log(typeof (answers));
+// console.log(typeof (null));
+
+
+
+//Lesson 8
+
+
+// const category = "toys";
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Privet, ${user}`);
+
+
+
+//Lesson 9
+
+
+// console.log('arr' + " - object");
+// console.log(4 + "5");
+
+// let incr = 10,
+//   decr = 10;
+
+// // ++incr;
+// // --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5 % 2);
+
+// console.log(2 * 4 === 8);
+// console.log(2 + 4 * 2 === 8);
+// console.log(2 + 2 * 2 !== '6');
+
+// const isChecked = false,
+//   isClose = false;
+
+// // console.log(isChecked && isClose);
+// console.log(isChecked || !isClose);
+
+
+
+//Lesson 10
+
+
