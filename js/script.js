cddf07d7978da2033fa10ab7c43b9da79d2d385f
alexandr0 <example@gmail.com>
2022-04-04 17:09:41 +0200

    New lessons
    new js file for new course

--- js/script.js
+++ js/script.js
@@ -222,7 +222,7 @@
 // const anotherNum = ret();
 // console.log(anotherNum);
 
-//Function expression 
+//Function expression
 // const logger = function () {
 //     console.log('Hello World');
 // };
@@ -230,36 +230,285 @@
 // logger();
 
 // Стрелочная функция
-const calc = (a, b) => a + b;
+// const calc = (a, b) => a + b;
 
 
 
-// Lesson 17
+// // Lesson 17
 
 
-const str = "teSt";
+// const str = "teSt";
 
-// console.log(str[2]);
-// console.log(str[2] = 'd');
-// console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-console.log(str);
+// // console.log(str[2]);
+// // console.log(str[2] = 'd');
+// // console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str);
 
-const fruit = "Some fruit";
-console.log(fruit.indexOf("fruit")); // поиск под строки
+// const fruit = "Some fruit";
+// console.log(fruit.indexOf("fruit")); // поиск под строки
 
-const logg = "Hello World";
+// const logg = "Hello World";
 
-// console.log(logg.slice(6, 11)); // до какого то момента не включая 
-console.log(logg.slice(-5, -1));
+// // console.log(logg.slice(6, 11)); // до какого то момента не включая
+// console.log(logg.slice(-5, -1));
 
-// console.log(logg.substring(6, 11)); // не поддерживает отрицательные значения
+// // console.log(logg.substring(6, 11)); // не поддерживает отрицательные значения
 
-console.log(logg.substr(6, 5)); // второй аргумент указывает сколько символов отрезать
+// console.log(logg.substr(6, 5)); // второй аргумент указывает сколько символов отрезать
 
-const num = 12.2;
-console.log(Math.round(num));
+// const num = 12.2;
+// console.log(Math.round(num));
 
-const test = "12.2px";
-// console.log(parseInt(test));
-console.log(parseFloat(test));
+// const test = "12.2px";
+// // console.log(parseInt(test));
+// console.log(parseFloat(test));
+
+
+
+// Lesson 19
+
+
+// function first() {
+//   //Do something
+//   setTimeout(function () {
+//     console.log(1);
+//   }, 500);
+// }
+
+// function second() {
+//   console.log(2);
+// }
+
+// first();
+// second();
+
+// //callback функция - это функция которая должна быть выполнена после того как другая функция завершила своё выполнение
+
+// function learnJS(lang, callback) {
+//   console.log(`I learn: ${lang}`);
+//   callback();
+// }
+
+// function done() {
+//   console.log('I done this lesson');
+// }
+
+// learnJS('JavaScript', done);
+
+
+
+// Lesson 20
+
+
+// const obj = new Object();
+
+// const options = {
+//   name: 'test',
+//   width: 1024,
+//   height: 1024,
+//   colors: {
+//     border: 'black',
+//     bg: 'red'
+//   },
+//   makeTest: function () {
+//     console.log("Test");
+//   }
+// };
+
+// options.makeTest();
+
+// const { border, bg } = options.colors;
+// console.log(border);
+
+// console.log(Object.keys(options).length);
+
+// console.log(options["colors"]["border"]);
+
+// delete options.name;
+
+// console.log(options);
+// let counter = 0;
+// for (let key in options) {
+//   if (typeof (options[key]) === 'object') {
+//     for (let i in options[key]) {
+//       console.log(`Option ${i} have item ${options[key][i]}`);
+//     }
+//   } else {
+//     console.log(`Option ${key} have item ${options[key]}`);
+//     counter++;
+//   }
+// }
+// console.log(counter);
+
+
+
+// Lesson 21
+
+
+// const arr = [11, 23, 35, 62, 8];
+// arr.sort(compareNum);
+// arr[99] = 0;
+// console.log(arr.length);
+// console.log(arr);
+
+// function compareNum(a, b) {
+//   return a - b;
+// }
+
+// arr.forEach(function (item, i, arr) {
+//   console.log(`${i}: ${item} inside massive ${arr}`);
+// });
+
+// arr.pop();
+// arr.push(10);
+
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//   console.log(value);
+// }
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join('; '));
+
+
+
+// Lesson 22
+
+
+// let a = 5,
+//   b = a;
+
+// b = b + 5;
+
+// console.log(b);
+// console.log(a);
+
+// const obj = {
+//   a: 5,
+//   b: 1
+// };
+
+// const copy = obj; // Ссылку
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+// function copy(mainObj) {
+//   let objCopy = {};
+
+//   let key;
+//   for (key in mainObj) {
+//     objCopy[key] = mainObj[key];
+//   }
+
+//   return objCopy;
+// }
+
+// const numbers = {
+//   a: 2,
+//   b: 5,
+//   c: {
+//     x: 7,
+//     y: 4
+//   }
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 10;
+// newNumbers.c.x = 10;
+
+// console.log(newNumbers);
+// console.log(numbers);
+
+// const add = {
+//   d: 17,
+//   i: 20
+// };
+
+// const clone = Object.assign({}, add);
+
+// clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+// const oldArray = ['a', 'b', 'c'];
+// const newArray = oldArray.slice();
+
+// newArray[1] = 'adasdasdas';
+// console.log(newArray);
+// console.log(oldArray);
+
+// const video = ['youtube', 'vimeo', 'rutube'],
+//   blogs = ['wordpress', 'livejournal', 'blogger'],
+//   internet = [...video, ...blogs, 'vk', 'facebook'];
+
+// console.log(internet);
+
+// function log(a, b, c) {
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+// }
+
+// const num = [2, 5, 7];
+// log(...num);
+
+// const array = ["a", "b"];
+
+// const newAarray = [...array];
+
+// const q = {
+//   one: 1,
+//   two: 2
+// };
+
+// const newObj = { ...q };
+// newObj.one = 2;
+// console.log(newObj);
+// console.log(q);
+
+
+
+// Lesson 22
+
+
+let str = "some";
+let strObj = new String(str);
+
+// console.log(typeof(strObj));
+// console.log(typeof(str));
+
+console.dir([1, 2, 3]);
+
+const soldier = {
+  health: 400,
+  armor: 100,
+  sayHello: function () {
+    console.log("Hello");
+  }
+};
+
+const jonh = Object.create(soldier);
+
+// const jonh = {
+//   health: 100
+// };
+
+// jonh.__proto__ = soldier;
+
+// Object.setPrototypeOf(jonh, soldier);
+
+// console.log(jonh.armor);
+jonh.sayHello();
